cf3d548fe1e052d34d8d933d37d2267867f9e3c1
Upendra Nilupul <139964152+UpendraNilupul@users.noreply.github.com>
2025-06-28 15:39:55 +0530

    feat: Add user management features with form and table
    - Updated package.json to include MUI and Emotion dependencies.
    - Implemented UserForm component for adding new users.
    - Created UserTable component to display a list of users.
    - Added Users component to manage user form and table.
    - Integrated routing to navigate between main app and users page.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-
+import { useNavigate } from 'react-router-dom';
 import './App.css';
 
 function App() {
+  const navigate = useNavigate();
   return (
     <div className="App">
       <header className="App-header">
         <h1>Welcome to react app</h1>
-        <button className='user-button'>User</button>
+        <button className='user-button' onClick={() => navigate('/users')}>User</button>
        
       </header>
     </div>
